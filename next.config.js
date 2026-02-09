271bdec434eb76b8f28b9c50218f9cff7fae6492
member4-Infinog <249049250+beingsamraj@users.noreply.github.com>
2026-02-09 22:02:50 +0530

    Enable static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    output: "export",       // makes static files
+    images: { unoptimized: true }, // required for GitHub Pages
+  };
+  
+  module.exports = nextConfig;
+  
